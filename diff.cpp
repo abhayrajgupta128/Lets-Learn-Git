f617fe07aaf5ee97204d3a371c70e84b693bb676
Abhay Raj Gupta <abhayrajgupta128@gmail.com>
2023-06-04 00:48:35 +0530

    added implementation for diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -1,4 +1,13 @@
 #include<iostream>
+using namespace std;
+
+int main()
 {
-    
+    int a,b,c;
+    cout<<"Enter two no.\n";
+    cin>>a>>b;
+
+    c=a-b;
+    cout<<"The diff of two no is : "<<c;
+    return 0;
 }
